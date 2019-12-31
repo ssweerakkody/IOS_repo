aefc9c9e3321774cb063ccedd2bb2dff45fd31ba
Suneth <dse16137@student.nibm.lk>
2019-12-31 22:16:00 +0530

    Login message is changed

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
      @IBAction func showMessage(sender: UIButton) {
-        let alertController = UIAlertController(title: "Authentication", message: "Login is successful !", preferredStyle: UIAlertController.Style.alert)
+        let alertController = UIAlertController(title: "Authentication", message: "Login is Successful !", preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
