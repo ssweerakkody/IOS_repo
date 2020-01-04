f9f44e479c252ec62bcce46b6d69988ec63e7e4e
Suneth <dse16137@student.nibm.lk>
2020-01-04 22:19:54 +0530

    colors are added to background color

---
+++ Navigation/Navigation/BlueViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BlueViewController.swift
+//  Navigation
+//
+//  Created by Suneth on 1/4/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    
+    var color : String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+                if (color == "RED") {
+                    self.view.backgroundColor = .red
+                    
+                    //self.view.navigationItem.
+                }
+        
+                if (color == "BLUE") {
+                    self.view.backgroundColor = .blue
+                    //self.view.navigationItem.
+                }
+        
+      
+        
+    }
+    
+    
+    
+}
