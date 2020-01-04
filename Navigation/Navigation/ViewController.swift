8a66a659ee40276b1fc9969fe377f5ba8617bf37
Suneth <dse16137@student.nibm.lk>
2020-01-04 20:33:36 +0530

    files are added

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Suneth on 1/1/20.
+//  Copyright © 2020 Suneth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
+
